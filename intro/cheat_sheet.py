b5b390855bd8a46656a0e5339c9de11d589e349c
BrandonAllenGreen <green04@gmail.com>
2018-06-28 12:53:37 -0500

    ignored examples.py and added some definitions to cheat_sheet

--- intro/cheat_sheet.py
+++ intro/cheat_sheet.py
@@ -1,3 +1,5 @@
+#******************** COLLECTION TYPES ********************#
+
 # lists are like JS arrays
 junk = list()
 junk = ['carrots', 'celery', 'kale', 2, ['peas', 'corn']]
@@ -44,17 +46,71 @@ display_name('Josephina')
 print(display_name.__doc__)
 
 
-# list comprehension is similar to map() and filter() in JS.  iterate over collection, perform logic, and return new collection.
+#******************** LOOPING ********************#
+# basic for loops are slower than comprehensions
+
+
+#///// lists /////#
 flowers = ['Lily', 'Snapdragon', 'Rose', 'Tulip']
 
-# long way to write a  for loop
 large_flowers = list()
 for f in flowers:
     large_flowers.append('a large ' + f)
 
 print(large_flowers)
 
-# shorthand for loop
+
+# ///// dictionaries /////#
+# a for loop on a dictionary will loop over the keys
+family = {'mother': 'Margaret', 'father': 'Reginald', 'sister': 'Jenny'}
+
+for f in family:
+    print(f)
+
+# if you want both the keys and values, loop over the items() property
+family = {'mother': 'Margaret', 'father': 'Reginald', 'sister': 'Jenny'}
+
+for key, value in family.items():
+    print(key, value)
+
+
+#///// sets /////#
+# looping over sets is identical to lists
+cars = {'Lexus', 'Chevy', 'Jeep'}
+
+for c in cars:
+    print(c)
+
+
+#******************** COMPREHENSION ********************#
+# python comprehension is similar to map() and filter() in JS.  iterate over collection, perform logic, and return new collection.
+# comprehension is faster than using a plain for loop and accomplishes same result.
+
+
+#///// lists /////#
+flowers = ['Lily', 'Snapdragon', 'Rose', 'Tulip']
+
 large_flowers = ['a large ' + f for f in flowers]
 
 print(large_flowers)
+
+#///// dictionaries /////#
+family = {
+    'mother': 'Jenine',
+    'father': 'Bo',
+    'brother': 'Taylor'
+}
+
+# need to use the items() property on the original dictionary for the iteration.
+my_family = {'my ' + key + ' is ' + value for (key, value) in family.items()}
+
+print(my_family)
+
+
+#///// sets /////#
+# set comprehensions are almost identical to list comprehensions.  punctuation is different
+possible_ratings = set(range(100))
+
+funky_ratings = {r/2 for r in possible_ratings}
+
+print(funky_ratings)
